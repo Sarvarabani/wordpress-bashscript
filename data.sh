c2ce7c9dacbfb0a26527e00d166f0b496eb35400
Sarvarabani <122893981+Sarvarabani@users.noreply.github.com>
2023-12-07 11:05:54 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -7,6 +7,4 @@ sudo  usermod -aG docker ec2-user
 sudo chmod 666 /var/run/docker.sock
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname-s)-$(uname-m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-git clone https://github.com/KESAVAGAJJALA/word-press-application.git
-cd wordpress1
 docker-compose up -d 
