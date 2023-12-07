a3c8aba9dffaa732dce6d9f41f1438de640be2e3
Sarvarabani <122893981+Sarvarabani@users.noreply.github.com>
2023-12-07 11:13:34 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo yum install -y git
 sudo yum install-y docker
 sudo systemctl start docker
 sudo systemctl enable docker
@@ -7,4 +6,6 @@ sudo  usermod -aG docker ec2-user
 sudo chmod 666 /var/run/docker.sock
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname-s)-$(uname-m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose up -d 
+cd wordpress
+sudo docker-compose up -d
+
